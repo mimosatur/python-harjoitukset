8e3c337dda80f20209b734eefd83bc77f7a2b268
Mimosa <Mimosa.Turunen@metropolia.fi>
2026-08-24 22:19:06 +0300

    teht 5 tehty loppuun

--- Peliprojekti/peliteht1.py
+++ Peliprojekti/peliteht1.py
@@ -1,3 +1,2 @@
 nimi = input("Mikä on nimesi: ")
 ika = input("Mikä on ikäsi: ")
-print("nimi", nimi, "ikä", ika)
--- mod03/tehtavat.py
+++ mod03/tehtavat.py
@@ -49,7 +49,10 @@ a = float(input("Anna leiviskät:\n"))
 b = float(input("Anna naulat:\n"))
 c = float(input("Anna luodit:\n"))
 
-print(f"Massa nykymittojen mukaan:\n {(a * 8.5) + (b * 0.425) + (c * 0.0133)} kilogrammaa ja {(a * 8500) + (b * 425) + (c * 13.3)} grammaa")
+kilogrammaa = (a * 8.5) + (b * 0.425) + (c * 0.0133)
+grammaa = (a * 8500) + (b * 425) + (c * 13.3)
+
+print(f"Massa nykymittojen mukaan:\n{int(kilogrammaa)} kilogrammaa ja {grammaa} grammaa")
 
 # tehtävä 6
 
