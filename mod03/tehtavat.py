3b0f3a8887dfedd4f8c88fb501e9f6d074c32694
Mimosa <Mimosa.Turunen@metropolia.fi>
2026-08-24 18:46:21 +0300

    korjauksia ja tein teht nro 2 ja 3

--- mod03/tehtavat.py
+++ mod03/tehtavat.py
@@ -1,18 +1,32 @@
 import math
 import random
 
-print("Hello, world!")
 
 # tehtävä 1
 
-nimi = input("Anna nimesi: ")
+nimi = input("Mika on nimesi: ")
 print(f"Terve, {nimi}!")
 
 # tehtävä 2
-
-# ympyrän pinta-ala: A = pi * r'2 
-r = float(input("Anna säde niin lasken ympyrän pinta-alan"))
+ 
+r = float(input("Anna säde niin lasken ympyrän pinta-alan: "))
 # r = float(r)
-print(r)
+# ympyrän pinta-ala: A = pi * r'2
 A = math.pi * r ** 2
-print(f"ympyrän pinta-ala on {A:.2f}")
+print(f"ympyrän pinta-ala on {A:.2f} yksikköä")
+
+
+# tehtävä 3
+
+a = float(input("Anna suorakulmion kanta: "))
+b = float(input("Anna suorakulmion korkeus: "))
+
+# suorakulmion piiri: p = 2a + 2b
+p = 2 * (a + b) 
+# suorakulmion pinta-ala: A = a * b
+A = a * b
+
+print(f"Suorakulmion piiri on: {p:.2f} ja pinta-ala {A:.2f}")
+print(f"Suorakulmion piiri on: {p:.2f} ja pinta-ala {(a*b):.2f}")
+
+# tehtävä 4
