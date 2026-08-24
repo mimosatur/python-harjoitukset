42593c8f089073704885b4348908dc174ae00c70
Mimosa <Mimosa.Turunen@metropolia.fi>
2026-08-24 12:59:22 +0300

    tein teht 2 ja 3

--- mod03/tehtavat.py
+++ mod03/tehtavat.py
@@ -1,3 +1,18 @@
-
+import math
+import random
 
 print("Hello, world!")
+
+# tehtävä 1
+
+nimi = input("Anna nimesi: ")
+print(f"Terve, {nimi}!")
+
+# tehtävä 2
+
+# ympyrän pinta-ala: A = pi * r'2 
+r = float(input("Anna säde niin lasken ympyrän pinta-alan"))
+# r = float(r)
+print(r)
+A = math.pi * r ** 2
+print(f"ympyrän pinta-ala on {A:.2f}")
