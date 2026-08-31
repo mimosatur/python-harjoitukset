b6ebc6b48094f0acefdd0ea9acc068c332a00e9e
Mimosa <Mimosa.Turunen@metropolia.fi>
2026-08-31 19:40:24 +0300

    alotin tehtävää 3 ja 5 ja tein teht 4

--- mod05/mod05teht.py
+++ mod05/mod05teht.py
@@ -1,3 +1,5 @@
+import random
+
 # Tehtävä 1
 
 luku = 1
@@ -16,7 +18,48 @@ tuuma = float(input("Anna tuumat: "))
 
 senttimetri = tuuma * 2.54
 
-while senttimetri >= 0:
-    print(f"{tuuma} tuumaa on senttimetreinä {senttimetri} cm")
-    if senttimetri < 0:
-        print("loppu")
+while tuuma >= 0:
+    print(f"{tuuma} tuumaa on senttimetreinä {float(senttimetri)} cm")
+    tuuma = float(input("Anna tuumat: "))
+    senttimetri = tuuma * 2.54
+    if tuuma < 0:
+        tuuma -= 1
+
+
+# Tehtävä 3
+
+luku = float(input("Anna luku: "))
+
+while luku != "":
+    luku = float(input("Anna luku: "))
+    if luku == "":
+        luku += 1
+
+
+# Tehtävä 4
+
+luku = random.randint(1,10)
+
+arvaus = int(input("Arvaa luku 1 ja 10 välillä: "))
+
+while arvaus < luku:
+    print("Liian pieni arvaus")
+    arvaus = int(input("Arvaa uudestaan: "))
+    if arvaus > luku:
+        print("Liian suuri arvaus")
+        arvaus = int(input("Arvaa uudestaan: "))
+    elif arvaus == luku:
+        print("Oikein")
+
+
+# Tehtävä 5
+
+oikea_käyttäjätunnus = "python"
+oikea_salasana = "rules"
+
+arvaus1 = str(input("Anna käyttäjätunnus: "))
+arvaus2 = str(input("Anna salasana: "))
+
+while arvaus1 != oikea_käyttäjätunnus and arvaus2 != oikea_salasana:
+    arvaus1 = str(input("Anna käyttäjätunnus uudelleen: "))
+    arvaus2 = str(input("Anna käyttäjätunnus uudelleen: "))
