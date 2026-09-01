824e6f0ba01dd525af984f347cb0e7d746ddb784
Mimosa <Mimosa.Turunen@metropolia.fi>
2026-09-01 16:27:04 +0300

    aloitin peliprojektitehtävän nro 2

---
+++ Peliprojekti/peliteht.py
@@ -0,0 +1,10 @@
+
+
+nimi = input("Mikä on nimesi: ")
+ika = input("Mikä on ikäsi: ")
+
+while ika < 12:
+    print("Olet alaikäinen")
+    if ika > 12:
+        print(f"Tervetuloa {nimi}")
+
--- Peliprojekti/peliteht1.py
+++
@@ -1,4 +0,0 @@
-nimi = input("Mikä on nimesi: ")
-ika = input("Mikä on ikäsi: ")
-
-print(f"Pelaajan nimi ja ikä: {nimi} {ika}")
