b7230bbb6c0342211ad95644867933e2245e443a
Mimosa <Mimosa.Turunen@metropolia.fi>
2026-09-19 19:55:16 +0300

    peliprojektin readme sisällön päivitys

--- Peliprojekti/peliteht.py
+++ Peliprojekti/peliteht.py
@@ -7,18 +7,19 @@ if ika < 12:
     print("Olet alaikäinen")
 
 else:
-    print(f"Tervetuloa peliin {nimi}!\n")
+    print(f"Tervetuloa Aamuruskon lehto peliin {nimi}!\n")
     
     peli_käynnissä = True
     while peli_käynnissä:
         print("Päävalikko:\nAloita peli\nAsetukset\nLopeta")
         valinta = input("\nValitse mitä haluat tehdä: ")
-    if valinta == "Aloita peli":
-        print("Peli aloitettu\n")
-    elif valinta == "Asetukset":
-        print("Asetukset\n")
-    elif valinta == "Lopeta":
-        print("Lopetit pelin")
+        if valinta == "Aloita peli":
+            print("Peli aloitettu\n")
+        elif valinta == "Asetukset":
+            print("Asetukset\n")
+        elif valinta == "Lopeta":
+            print("Lopetit pelin")
         peli_käynnissä = False
+
 
 
--- Peliprojekti/readme.md
+++ Peliprojekti/readme.md
@@ -1,4 +1,10 @@
-## Jalokivi
+## Aamuruskon lehto
 **Mimosa Turunen
+
+Kestävän kehityksen tavoite: 
+    nro 15, Maanpäällinen elämä
+
+Pelin tavoite:
+    Palauttaa metsä ennalleen laittoman hakkuun jälkeen
 
 
