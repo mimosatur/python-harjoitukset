08f152f90af0aaae3aa676527f110093600862a1
Mimosa <Mimosa.Turunen@metropolia.fi>
2026-09-21 17:01:14 +0300

    tein pelitehtävä 3

--- Peliprojekti/peliteht.py
+++ Peliprojekti/peliteht.py
@@ -3,23 +3,41 @@
 nimi = input("Mikä on nimesi: ")
 ika = int(input("Kuinka vanha olet: "))
 
+reppu = []
+
+def inventaario():
+    esine = input("Anna esine: ")
+    reppu.append(esine)
+    return
+
+def tulosta():
+    return print(reppu)
+
+def lopetus():
+    return print("Nähdään ensikerralla!")
+
 if ika < 12:
     print("Olet alaikäinen")
 
 else:
     print(f"Tervetuloa Aamuruskon lehto peliin {nimi}!\n")
     
-    peli_käynnissä = True
-    while peli_käynnissä:
+    
+    while True:
         print("Päävalikko:\nAloita peli\nAsetukset\nLopeta")
         valinta = input("\nValitse mitä haluat tehdä: ")
         if valinta == "Aloita peli":
             print("Peli aloitettu\n")
+            inventaario()
         elif valinta == "Asetukset":
             print("Asetukset\n")
+            tulosta()
+            print()
         elif valinta == "Lopeta":
             print("Lopetit pelin")
-        peli_käynnissä = False
+            lopetus()
+            break
+    
         
 
 
